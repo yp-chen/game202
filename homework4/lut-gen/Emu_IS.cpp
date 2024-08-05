0e297d7154793a85afa4390965bc3be44e9512a9
Yongpeng Chen <yongpeng.chen@momenta.ai>
2024-08-05 15:03:30 +0800

    4-Emu_IS

--- homework4/lut-gen/Emu_IS.cpp
+++ homework4/lut-gen/Emu_IS.cpp
@@ -13,6 +13,11 @@
 
 const int resolution = 128;
 
+//这个函数是为了生成Hammersley序列
+//Hammersley 是用来生成在单位平方内均匀分布的点序列的。
+//Hammersley序列是一个低差异（quasi-random）的序列，与伪随机数相比，在多维采样时能更均匀地覆盖采样区域。
+//它常用于计算机图形学中，特别是光照渲染技术如蒙特卡洛积分中，因为它可以减少采样噪声并加快收敛速度。
+//返回：[0, 1) x [0, 1),得到的就是公式θ m = arctan((α √ ξ1)/(√ 1 − ξ1))   ϕ h = 2πξ2中的ξ1和ξ2
 Vec2f Hammersley(uint32_t i, uint32_t N) { // 0-1
     uint32_t bits = (i << 16u) | (i >> 16u);
     bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
@@ -23,29 +28,48 @@ Vec2f Hammersley(uint32_t i, uint32_t N) { // 0-1
     return {float(i) / float(N), rdi};
 }
 
+//重要性采样GGX
+//通过给定的随机变量 Xi（由Hammersley序列生成）法线 N，以及粗糙度 roughness 来生成一个半球内的采样向量。
 Vec3f ImportanceSampleGGX(Vec2f Xi, Vec3f N, float roughness) {
     float a = roughness * roughness;
 
     //TODO: in spherical space - Bonus 1
-
+    //得到球坐标系下的theta和phi
+    float theta = atan(a * sqrt(Xi.x) / sqrt(1.0f - Xi.x));
+    float phi = 2.0f * PI * Xi.y;
 
     //TODO: from spherical space to cartesian space - Bonus 1
- 
+    //得到对应的三维坐标
+    float sinTheta = sin(theta);
+    float cosTheta = cos(theta);
+    Vec3f H = Vec3f(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
 
     //TODO: tangent coordinates - Bonus 1
-
+    //得到切线坐标系
+    Vec3f up = abs(N.z) < 0.999 ? Vec3f(0.0, 0.0, 1.0) : Vec3f(1.0, 0.0, 0.0);
+    Vec3f tangent = normalize(cross(up, N));
+    Vec3f bitangent = cross(N, tangent);
 
     //TODO: transform H to tangent space - Bonus 1
-    
-    return Vec3f(1.0f);
+    //将H从世界坐标系转换到切线坐标系
+    return normalize(tangent * H.x + bitangent * H.y + N * H.z);
 }
 
+//得到brdf的FDG项的G项中的Schlick项
+//NdotV:法线与视角的夹角 roughness:粗糙度
 float GeometrySchlickGGX(float NdotV, float roughness) {
     // TODO: To calculate Schlick G1 here - Bonus 1
-    
-    return 1.0f;
+    float a = roughness;
+    float k = (a * a) / 2.0f;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0f - k) + k;
+
+    return nom / denom;
 }
 
+//得到brdf的FDG项的G项
+//roughness:粗糙度 NoV:法线与视角的夹角 NoL:法线与光线的夹角
 float GeometrySmith(float roughness, float NoV, float NoL) {
     float ggx2 = GeometrySchlickGGX(NoV, roughness);
     float ggx1 = GeometrySchlickGGX(NoL, roughness);
@@ -53,13 +77,18 @@ float GeometrySmith(float roughness, float NoV, float NoL) {
     return ggx1 * ggx2;
 }
 
+//V:视角，也就是公式中的o roughness:粗糙度
 Vec3f IntegrateBRDF(Vec3f V, float roughness) {
 
     const int sample_count = 1024;
+    float A = 0.0;
+    //法线，也就是公式中的n
     Vec3f N = Vec3f(0.0, 0.0, 1.0);
     for (int i = 0; i < sample_count; i++) {
         Vec2f Xi = Hammersley(i, sample_count);
+        //也就是公式中的m和h
         Vec3f H = ImportanceSampleGGX(Xi, N, roughness);
+        //光照方向，也就是公式中的i
         Vec3f L = normalize(H * 2.0f * dot(V, H) - V);
 
         float NoL = std::max(L.z, 0.0f);
@@ -67,14 +96,14 @@ Vec3f IntegrateBRDF(Vec3f V, float roughness) {
         float VoH = std::max(dot(V, H), 0.0f);
         float NoV = std::max(dot(N, V), 0.0f);
         
-        // TODO: To calculate (fr * ni) / p_o here - Bonus 1
-
-
+        // TODO: To calculate (fr * ni) / p_o here - Bonus 1'
+        float G = GeometrySmith(roughness, NoV, NoL);
+        A += (VoH * G) / (NoV * NoH);
         // Split Sum - Bonus 2
         
     }
 
-    return Vec3f(1.0f);
+    return Vec3f(A/sample_count, A/sample_count, A/sample_count); // No split sum version
 }
 
 int main() {
@@ -88,13 +117,13 @@ int main() {
 
             Vec3f irr = IntegrateBRDF(V, roughness);
 
-            data[(i * resolution + j) * 3 + 0] = uint8_t(irr.x * 255.0);
-            data[(i * resolution + j) * 3 + 1] = uint8_t(irr.y * 255.0);
-            data[(i * resolution + j) * 3 + 2] = uint8_t(irr.z * 255.0);
+            data[(i * resolution + j) * 3 + 0] = uint8_t((1-irr.x) * 255.0);
+            data[(i * resolution + j) * 3 + 1] = uint8_t((1-irr.y) * 255.0);
+            data[(i * resolution + j) * 3 + 2] = uint8_t((1-irr.z) * 255.0);
         }
     }
-    stbi_flip_vertically_on_write(true);
-    stbi_write_png("GGX_E_LUT.png", resolution, resolution, 3, data, resolution * 3);
+    // stbi_flip_vertically_on_write(true);
+    stbi_write_png("GGX_E_LUT_2.png", resolution, resolution, 3, data, resolution * 3);
     
     std::cout << "Finished precomputed!" << std::endl;
     return 0;
